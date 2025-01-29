72add93c660234fae032828b5f05fe3bddc85d81
SYED KASHIF <skashif00974@gmail.com>
2025-01-29 19:47:40 +0530

    p

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5050/",
+  baseURL: "https://stackoverflow-rlt4.onrender.com/",
 });
 
 API.interceptors.request.use((req) => {
